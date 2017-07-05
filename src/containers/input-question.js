184e9ec9c2bbdf42f6f7274cf691a6e88728b3c9
Didac Esteban <didacestebanandres@Didacs-MacBook-Pro.local>
2017-07-05 22:56:37 +0200

    removing useless functions from example

--- src/containers/input-question.js
+++ src/containers/input-question.js
@@ -11,16 +11,10 @@ class InputQuestion extends Component {
     this.state = {value: ''};
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
   this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
   }
 
  render() {
