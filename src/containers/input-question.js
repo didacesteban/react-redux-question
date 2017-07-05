7e5b89c7221e0610715c30af03bfc983ec157df8
Didac Esteban <didacestebanandres@Didacs-MacBook-Pro.local>
2017-07-05 22:55:28 +0200

    getting data from input

--- src/containers/input-question.js
+++ src/containers/input-question.js
@@ -3,13 +3,32 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {sendQuestion} from '../actions/index'
 
+
 class InputQuestion extends Component {
-    render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+  this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+ render() {
       let question = 'question test';
         return (
             <div>
-              <input type="text" />
-              <button onClick={() => this.props.sendQuestion(question)}>send</button>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+              <button onClick={() => this.props.sendQuestion(this.state.value)}>send</button>
               <hr/>
               <h2>{this.props.question}</h2>
             </div>
