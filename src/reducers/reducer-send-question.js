c21187e961b5b099842218c7a1538539c2c80d19
Didac Esteban <didacestebanandres@Didacs-MacBook-Pro.local>
2017-07-05 23:14:28 +0200

    answering questions

--- src/reducers/reducer-send-question.js
+++ src/reducers/reducer-send-question.js
@@ -1,7 +1,16 @@
 export default function (state = null, action) {
     switch (action.type) {
         case 'SEND_QUESTION':
-            return action.payload;
+          if(action.payload.substr(-1) != '?'){return 'Type a question please';}
+          let  number = Math.floor((Math.random() * 3) + 1);
+          switch (number) {
+            case 1:
+              return 'Yes';
+            case 2:
+              return 'No';
+            case 3:
+              return 'Maybe';
+          }
     }
     return state;
 }
