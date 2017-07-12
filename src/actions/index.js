d8d82bdef9f143b2fd9571f8e8330d33edf3dd51
Didac Esteban <didacesteban@MacBook-Pro-de-Daniel.local>
2017-07-12 09:59:45 +0200

    exporting actions using the same method than redux official documentation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
-export const sendQuestion = (question) => {
+export const SEND_QUESTION = 'SEND_QUESTION'
+
+export function sendQuestion(question) {
     return {
         type: 'SEND_QUESTION',
         payload: question
     }
-};
+}
