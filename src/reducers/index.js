aa1e0b8220bba0d33502973bf9234b22a5067762
Didac Esteban <>
2019-05-30 15:53:05 +0200

    Updates

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import QuestionActions from './reducer-send-question';
-
-const allReducers = combineReducers({
-    questionActions: QuestionActions
-});
-
-export default allReducers
